5ad2a532fdea2b6b43279c82a5ee6b0f30d69548
VAIDANT <72136359+VAIDANT@users.noreply.github.com>
2020-12-27 17:19:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+canvas =
+document.getElementById("myCanvas");
+ctx= canvas.getContext("2d")
+
+ctx.beginPath();
+ctx.strokeStyle = "red"
+ctx.lineWidth = 1;
+ctx.rect(200, 143, 430, 200)
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "blue";
+ctx.lineWidth = 5;
+ctx.arc(300, 210, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "black";
+ctx.lineWidth = 5;
+ctx.arc(400, 210, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "red";
+ctx.lineWidth = 5;
+ctx.arc(500, 210, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "yellow";
+ctx.lineWidth = 5;
+ctx.arc(350, 250, 40, 0, 2 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "green";
+ctx.lineWidth = 5;
+ctx.arc(450, 250, 40, 0, 2 * Math.PI);
+ctx.stroke();
